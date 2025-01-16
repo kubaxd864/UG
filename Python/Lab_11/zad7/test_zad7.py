457ed23541be51aa9dfc7a1d926e7d73359ce5fc
kubaxd864 <79090487+kubaxd864@users.noreply.github.com>
2025-01-16 16:43:25 +0100

    Update

--- Python/Lab_11/zad7/test_zad7.py
+++ Python/Lab_11/zad7/test_zad7.py
@@ -1,7 +1,4 @@
 from zad7 import bubble_sort
 
-def test_bubble_sort_1():
-    assert bubble_sort([5, 4, 3, 2, 1]) == [1, 2, 4, 3, 5]
-
-def test_bubble_sort_2():
+def test_bubble_sort():
     assert bubble_sort([5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5]
