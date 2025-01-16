1f8409ae4ac388a397ce9a477ac193544437250b
kubaxd864 <79090487+kubaxd864@users.noreply.github.com>
2025-01-16 16:40:43 +0100

    Update

--- Python/Lab_11/zad4/test_zad4.py
+++ Python/Lab_11/zad4/test_zad4.py
@@ -1,4 +1,4 @@
 from zad4 import silnia
 
 def test_silnia():
-    assert silnia(4) == 120
+    assert silnia(4) == 24
--- Python/Lab_11/zad5/test_zad5.py
+++ Python/Lab_11/zad5/test_zad5.py
@@ -2,7 +2,7 @@ from zad5 import in_table
 from zad5 import tablica
 
 def test_in_table_0():
-    assert in_table(0, tablica)
+    assert in_table(1, tablica)
 
 def test_in_table_3():
     assert in_table(3, tablica)
@@ -11,4 +11,4 @@ def test_in_table_5():
     assert in_table(5, tablica)
 
 def test_in_table_13():
-    assert in_table(13, tablica)
+    assert in_table(11, tablica)
